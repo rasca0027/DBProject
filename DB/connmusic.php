ab283f3d831c66a9b7684dd2f760647280fa6649
every85713 <every85713@gmail.com>
2014-01-14 16:33:16 +0800

    mysqli
    只有這東西需要改而已啊...

--- DB/connmusic.php
+++ DB/connmusic.php
@@ -4,9 +4,9 @@
 	$db_username = "root";
 	$db_password = "root";
 	//連線伺服器
-	$db_connect = @mysql_connect($db_host, $db_username, $db_password);
+	$db_connect = @mysqli_connect($db_host, $db_username, $db_password);
 	if(!$db_link) die("<p>資料連結失敗!</p>");
 	//設定字元集與連線校對
 	mysqli_set_charset($db_connect, "utf8");
-	mysql_select_db("music")  or die("<p>資料表選擇失敗</p>");
+	mysqli_select_db("music")  or die("<p>資料表選擇失敗</p>");
 ?>
